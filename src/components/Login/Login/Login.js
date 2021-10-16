091da669915a4fd4c2281dfb3cc04c584fa8b8ec
Abdul Hannan <shohas563@gmail.com>
2021-10-17 01:33:58 +0600

    css update login

--- src/components/Login/Login/Login.js
+++ src/components/Login/Login/Login.js
@@ -96,52 +96,53 @@ const Login = () => {
     }
 
     return (
-        <div className="text-center">
-            <h3>You Must Log In</h3>
-            <button className="inner-button-fix mt-5" onClick={handleGoogleSignIn}><FontAwesomeIcon icon={faGoogle} /> Google Sign In</button>
-            <div className="flex flex-col container w-full max-w-md  py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8">
-                <div className="self-center mb-6 text-xl font-light text-gray-600 sm:text-2xl dark:text-white">
-                    Login To Your Account
-                </div>
-                <div className="mt-8">
-                    <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
-                        <div className="flex flex-col mb-2">
-                            <div className="flex relative ">
-                                <input defaultValue="test@admin.com" {...register("email")} type="text" id="sign-in-email" className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Your email" />
+        <div className="text-center container mt-3">
+            <div className="form-fix">
+                <h3 className="mt-3">You Must Log In</h3>
+                <button className="inner-button-fix mt-3 mb-3" onClick={handleGoogleSignIn}><FontAwesomeIcon icon={faGoogle} /> Google Sign In</button>
+                <div>
+                    <div className="mb-3">
+                        Login With Email
+                    </div>
+                    <div className="mt-8">
+                        <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
+                            <div className="mb-2">
+                                <div className="flex relative ">
+                                    <input defaultValue="test@admin.com" {...register("email")} type="text" id="sign-in-email" className="border py-2 px-4" placeholder="Your email" />
+                                </div>
                             </div>
-                        </div>
-                        <div className="flex flex-col mb-6">
-                            <div className="flex relative ">
-                                <input defaultValue="123456" {...register("password")} type="password" id="sign-in-email" className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Your password" />
+                            <div className="flex flex-col mb-6">
+                                <div className="flex relative ">
+                                    <input defaultValue="123456" {...register("password")} type="password" id="sign-in-email" className="border py-2 px-4" placeholder="Your password" />
+                                </div>
                             </div>
-                        </div>
-                        <div className="flex items-center mb-6 -mt-4">
-                            <div className="flex ml-auto">
-                                <Link to="/" className="inline-flex text-xs font-thin text-gray-500 sm:text-sm dark:text-gray-100 hover:text-gray-700 dark:hover:text-white">
-                                    Forgot Your Password?
-                                </Link>
+                            <div className="flex items-center mb-6 -mt-4">
+                                <div className="flex ml-auto mb-2 mt-2">
+                                    <Link to="/">
+                                        Forgot Your Password?
+                                    </Link>
+                                </div>
                             </div>
-                        </div>
-                        <div class="flex w-full">
-                            <button type="submit" class="py-2 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none rounded">
-                                {loading ? <div className="spinner-border" role="status">
+                            <div class="flex w-full">
+                                <button type="submit" class="py-2 px-4 rounded">
+                                    {loading ? <div className="spinner-border" role="status">
                                         <span class="visually-hidden"></span>
                                     </div> : "Login"
-                                }
-                            </button>
-                        </div>
-                    </form>
-                </div>
-                {error && <span style={{ color: 'red', marginTop: '10px' }}>Something went wrong!</span>}
-                <div className="flex items-center justify-center mt-6">
-                    <Link to="/signup" className="inline-flex items-center text-xs font-thin text-center text-gray-500 hover:text-gray-700 dark:text-gray-100 dark:hover:text-white">
-                        <span className="ml-2">
-                            You don't have an account?
-                        </span>
-                    </Link>
+                                    }
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    {error && <span style={{ color: 'red', marginTop: '10px' }}>Something went wrong!</span>}
+                    <div className="mt-6">
+                        <Link to="/signup" className="">
+                            <span className="ml-2">
+                                You don't have an account?
+                            </span>
+                        </Link>
+                    </div>
                 </div>
             </div>
-
         </div>
     );
 };
--- src/components/Login/SignUp/SignUp.jsx
+++ src/components/Login/SignUp/SignUp.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { CgSpinner } from 'react-icons/cg';
 import axios from 'axios';
 import { useForm } from 'react-hook-form';
+import '../Login/Login.css';
 
 const SignUp = () => {
     const [error, setError] = useState(false);
@@ -36,42 +37,42 @@ const SignUp = () => {
     }
 
     return (
-        <div className="w-1/2">
-            <div class="flex flex-col max-w-lg px-4 py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10 mx-auto">
-                <div class="self-center mb-2 text-xl font-light text-gray-800 sm:text-2xl dark:text-white">
+        <div className="text-center container mt-3 ">
+            <div class="form-fix">
+                <h3 class="mt-4 mb-4">
                     User Signup
-                </div>
-                <span class="justify-center text-sm text-center text-gray-500 flex-items-center dark:text-gray-400">
-                    Already have an account ?
-                    <Link to="login" class="text-sm text-blue-500 underline hover:text-blue-700">
-                        Log in
-                    </Link>
-                </span>
+                </h3>
                 <div class="p-6 mt-8">
                     <form onSubmit={handleSubmit(onSubmit)}>
                         <div class="flex flex-col mb-2">
-                            <div class=" relative ">
-                                <input type="text" id="create-account-pseudo" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Your Full Name"
+                            <div class="">
+                                <input type="text" id="create-account-pseudo" class="border py-2 px-4" placeholder="Your Full Name"
                                     {...register("name")} />
                             </div>
                         </div>
                         <div class="flex flex-col mb-2">
-                            <div class=" relative ">
-                                <input type="email" id="create-account-pseudo" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Your Email Address"
+                            <div class="">
+                                <input type="email" id="create-account-pseudo" class="border py-2 px-4" placeholder="Your Email Address"
                                     {...register("email")} />
                             </div>
                         </div>
                         <div class="flex flex-col mb-2">
-                            <div class=" relative ">
-                                <input type="password" class="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder=" Password"
+                            <div class="">
+                                <input type="password" class="border py-2 px-4" placeholder=" Password"
                                     {...register("password")} />
                             </div>
                         </div>
-                        <div class="flex w-full my-4">
+                        <div class="flex w-full my-2">
 
-                            <button type="submit" class="py-2 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none rounded">
-                                {pending ?  <CgSpinner class="animate-spin text-xl"/> : "Sign up"}
+                            <button type="submit" class="border py-2 px-4 rounded">
+                                {pending ? <CgSpinner class="animate-spin text-xl" /> : "Sign up"}
                             </button>
+                        </div>
+                        <div class="mb-3">
+                            Already have an account ?
+                            <Link to="login" class="">
+                                Log in
+                            </Link>
                         </div>
                         {error && <span style={{ color: 'red', marginTop: '10px' }}>Something went wrong!</span>}
                     </form>
